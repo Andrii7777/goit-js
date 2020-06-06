5d6c3e10617c9330d3288109ac1d11be26146319
Andrii7777 <60316097+Andrii7777@users.noreply.github.com>
2020-06-07 01:00:39 +0300

    first commit

---
+++ js-hw-2/js/task-6.js
@@ -0,0 +1,23 @@
+let input;
+const numbers = [];
+let total = 0;
+
+while (input !== null) {
+  input = prompt("Введите число:");
+
+  if (input !== null) {
+    input = Number(input);
+
+    if (Number.isNaN(input)) {
+      alert("Было введено не число, попробуйте еще раз");
+      continue;
+    }
+    numbers.push(input);
+  }
+}
+if (numbers.length) {
+  for (const number of numbers) {
+    total += number;
+  }
+}
+console.log(`Общая сума чисел равна: ${total}`);
