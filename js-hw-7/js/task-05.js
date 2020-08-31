07332f361ef7fbb7da37ff8fe72f8dac451ad17e
Andrii7777 <60316097+Andrii7777@users.noreply.github.com>
2020-08-31 23:55:59 +0300

    dz-7

---
+++ js-hw-7/js/task-05.js
@@ -0,0 +1,10 @@
+const nameInputRef = document.querySelector("#name-input");
+const nameOutputRef = document.querySelector("#name-output");
+
+const nameChange = () => {
+  nameInputRef.value === ""
+    ? (nameOutputRef.textContent = "незнакомец")
+    : (nameOutputRef.textContent = nameInputRef.value);
+};
+
+nameInputRef.addEventListener("input", nameChange);
